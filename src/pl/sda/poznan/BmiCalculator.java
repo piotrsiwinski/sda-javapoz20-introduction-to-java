7865f1f4e7186fa5c243ab6a8ccae2873c782112
Piotr Siwinski <siwinski.piotr@outlook.com>
2019-08-24 14:18:17 +0200

    zadanie bmi

---
+++ src/pl/sda/poznan/BmiCalculator.java
@@ -0,0 +1,30 @@
+package pl.sda.poznan;
+
+import java.util.Scanner;
+
+public class BmiCalculator {
+    private static Scanner czytnik = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        System.out.println("Podaj swoj wzrost w metrach");
+        float wzrost = czytnik.nextFloat();
+        System.out.println("Podaj swoja wage w kg");
+        float waga = czytnik.nextFloat();
+        float bmi = waga / (float) Math.pow(wzrost, 2);
+        // 2 wersja obliczania bmi (bez funkcji pow):
+        // float bmi = waga / (wzrost * wzrost);
+
+        System.out.println(String.format("Twoje BMI to: %.2f", bmi));
+
+        if (bmi < 18.5) {
+            System.out.println("Niedowaga");
+        } else if (bmi >= 18.5 && bmi < 24.5) {
+            System.out.println("Waga ok");
+        } else if (bmi >= 24.5 && bmi < 27) {
+            System.out.println("Nadwaga");
+        } else if (bmi >= 27 && bmi < 29) {
+            System.out.println("Otylosc I stopnia");
+        }
+
+    }
+}
---
+++ src/pl/sda/poznan/StringFormatDemo.java
@@ -0,0 +1,17 @@
+package pl.sda.poznan;
+
+public class StringFormatDemo {
+    public static void main(String[] args) {
+        String imie = "Piotr";
+        String nazwisko = "Nowak";
+        int wiek = 20;
+        System.out.printf("imie to: %s, nazwisko to:%s, wiek to: %d%n", imie, nazwisko, wiek);
+
+        System.out.println(
+                String.format("imie to: %s, nazwisko to: %s, a wiek to: %d",
+                        nazwisko,
+                        imie,
+                        wiek));
+
+    }
+}
