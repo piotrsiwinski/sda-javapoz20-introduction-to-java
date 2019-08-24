3a8475688f1f13b5eacf00d9e262a7f47aa5f484
Piotr Siwinski <siwinski.piotr@outlook.com>
2019-08-24 15:16:43 +0200

    zadanie6

---
+++ src/pl/sda/poznan/Calculator.java
@@ -0,0 +1,69 @@
+package pl.sda.poznan;
+
+import java.util.Scanner;
+
+public class Calculator {
+
+    private static Scanner odczyt = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        System.out.println("Kalkulator");
+        System.out.println("Podaj dzialanie w formacie: liczba1 operacja liczba2 ");
+        System.out.println("Pamietaj o spacjach pomiedzy liczbami i znakiem operacji ");
+        System.out.println("Wpisz exit aby zakonczyc program");
+
+
+        while (true) {
+            System.out.println("Podaj dzialanie: ");
+
+            String dzialanie = odczyt.nextLine();
+            if (dzialanie.equals("exit")) {
+                break;
+            }
+
+            String[] tablica = dzialanie.split(" ");
+
+            double a = 0;
+            double b = 0;
+            String operacja;
+            if (tablica.length != 3){
+                System.out.println("Piotr");
+                continue;
+            }
+
+            try {
+                a = Double.parseDouble(tablica[0]);
+                operacja = tablica[1];
+                b = Double.parseDouble(tablica[2]);
+            } catch (NumberFormatException e) {
+                System.out.println("Zly format");
+                continue;
+            }
+
+
+            switch (operacja) {
+                case "+":
+                    System.out.println("Wynik to: " + (a + b));
+                    break;
+
+                case "-":
+                    System.out.println("Wynik to: " + (a - b));
+                    break;
+                case "*":
+                    System.out.println("Wynik to: " + (a * b));
+                    break;
+                case "/":
+                    if (b == 0) {
+                        System.out.println("Nie mozna dzielic przez 0");
+                    } else {
+                        System.out.println("Wynik to: " + (a / b));
+                    }
+                    break;
+                default:
+                    System.out.println("Bledny znak operacji...");
+                    break;
+            }
+        }
+    }
+
+}
